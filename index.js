a98dc949478d8c4839586ce82d0be6d649810e1a
nyxcoding <mmarleyapple@icloud.com>
2026-07-19 18:13:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const Database = require("better-sqlite3");
+const { ForgeClient } = require("@tryforge/forgescript")
+const { ForgeDB } = require("@tryforge/forge.db")
+const token = require("./token")
+
+const db = new ForgeDB({
+    events: [
+        "connect"
+    ],
+    type: "better-sqlite3"
+})
+
+const client = new ForgeClient({
+    intents: [
+        "GuildMessages",
+        "Guilds",
+        "MessageContent"
+    ],
+    events: [
+        "messageCreate",
+        "clientReady"
+    ],
+    prefixes:[
+        "!"
+    ],
+    extensions:[
+        db
+    ]
+})
+
+
+
+db.variables({
+  commandsUsed: 0,
+  python: 0,
+  javascript: 0,
+  typescript: 0,
+  html: 0,
+  about: 0
+})
+
+client.applicationCommands.load("commands/application")
+client.commands.load("commands/client")
+
+client.login(token)
